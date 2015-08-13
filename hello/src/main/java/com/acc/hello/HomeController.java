a8d3a1653ecb7a3b12e7cd06314e637b6d01d83f
Cristian Groza <cristy1126@gmail.com>
2015-08-13 16:09:57 +0300

    Hello
    inceput

---
+++ hello/src/main/java/com/acc/hello/HomeController.java
@@ -0,0 +1,35 @@
+package com.acc.hello;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(value="name", required=false, defaultValue="world") String name, Model model){
+		model.addAttribute("name", name);
+		return "hello";
+	}
+	
+	@RequestMapping("/")
+	public String root(Model model){
+		model.addAttribute("fp", "Prima pagina");
+		model.addAttribute("linkHello", "<a href=\"/hello\">Hello page</a>");
+		return "hello";
+		
+	}
+	
+}
