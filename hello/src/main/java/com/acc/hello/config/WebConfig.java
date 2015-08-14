5e8ab5a5b03606eb99ef4f4eba60b6ea6e1e0c38
Cristian Groza <cristy1126@gmail.com>
2015-08-14 16:09:39 +0300

    template resolver

--- hello/src/main/java/com/acc/hello/config/WebConfig.java
+++ hello/src/main/java/com/acc/hello/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.acc.hello.config;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,24 +12,34 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 @Configuration
 @EnableWebMvc
 public class WebConfig {
-	
 	@Bean
-	public ThymeleafViewResolver resolver(){
-		ThymeleafViewResolver view = new ThymeleafViewResolver();
-		return view;
-	}
-	
-	public SpringTemplateEngine template(){
-		SpringTemplateEngine template = new SpringTemplateEngine();
-		return template;
-	}
-	
 	public ServletContextTemplateResolver templateResolver(){
-		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
-		templateResolver.setPrefix("/WEB-INF/templates/");
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();		
+		
+		
+		templateResolver.setPrefix("/WEB-INF/templates");
 		templateResolver.setSuffix(".html");
 		templateResolver.setTemplateMode("HTML5");
 		
 		return templateResolver;
 	}
+	
+	@Bean
+	public SpringTemplateEngine template(){
+		SpringTemplateEngine template = new SpringTemplateEngine();
+		template.setTemplateResolver(templateResolver());
+		return template;
+	}
+	
+	@Bean
+	public ThymeleafViewResolver resolver(){
+		ThymeleafViewResolver view = new ThymeleafViewResolver();
+		view.setTemplateEngine(template());
+		return view;
+	}
+	
+	
+	
+ 
+	
 }
