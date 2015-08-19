385383cc2b62061799ff26a96542525a77d2ee07
razvantufisi <razvantufisi94@yahoo.ro>
2015-08-19 12:22:32 +0300

    css station

--- hello/src/main/java/com/acc/internship/controller/AdminController.java
+++ hello/src/main/java/com/acc/internship/controller/AdminController.java
@@ -3,7 +3,9 @@ package com.acc.internship.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminController {
@@ -14,5 +16,13 @@ public class AdminController {
 		
 		return "/admin?routes";
 	}
+	
+	   @RequestMapping(value="/admin?newstation", method=RequestMethod.GET)
+	    public String greetingForm(Model model) {
+	       // model.addAttribute("fetchroute", new setRoute());
+	        return "/admin?newstation";
+	    }
+
+	
 	
 }
