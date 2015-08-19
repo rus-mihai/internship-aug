f3dc34d64315380fe5c676b06b20a01cb8cb02e3
Cristian Groza <cristy1126@gmail.com>
2015-08-19 15:20:19 +0300

    DAO routes station
    DAO for route and station
    not tested

--- hello/src/main/java/com/acc/internship/repo/RouteDAO.java
+++ hello/src/main/java/com/acc/internship/repo/RouteDAO.java
@@ -1,5 +1,12 @@
 package com.acc.internship.repo;
 
-public interface RouteDAO {
+import java.util.List;
 
+import com.acc.internship.model.Route;
+
+public interface RouteDAO {
+	public Route get(int id);
+	public List<Route> list();
+	public void add(Route r);
+	public void delete(int id);
 }
---
+++ hello/src/main/java/com/acc/internship/repo/StationDAOImpl.java
@@ -0,0 +1,68 @@
+package com.acc.internship.repo;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.acc.internship.model.Route;
+import com.acc.internship.model.Station;
+
+@Repository
+public class StationDAOImpl implements StationDAO {
+	
+	@Autowired
+	private EntityManager entityManager;
+	
+	public StationDAOImpl(){
+		
+	}
+	
+	public StationDAOImpl(EntityManager em){
+		this.entityManager = em;
+	}
+	
+	@PersistenceContext
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
+	@Override
+	@Transactional
+	public Station get(int id) {
+		return getEntityManager().find(Station.class, id);
+	}
+
+	@Override
+	public List<Station> list() {
+		String hql = "from Station";
+		Query query = getEntityManager().createQuery(hql);
+		List<Station> stations = query.getResultList();
+		return stations;
+	}
+
+	@Override
+	@Transactional
+	public void add(Station r) {
+		Station station = get(r.getId());
+		if(station == null){
+			getEntityManager().persist(r);
+		}
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
