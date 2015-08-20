c526791c1b632f9d471787be07b05bb650fa8989
razvantufisi <razvantufisi94@yahoo.ro>
2015-08-20 14:49:52 +0300

    treaba de DB

--- hello/src/main/java/com/acc/internship/controller/AdminController.java
+++ hello/src/main/java/com/acc/internship/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.acc.internship.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.acc.internship.model.Station;
+import com.acc.internship.model.User;
 import com.acc.internship.repo.RouteDAO;
 import com.acc.internship.repo.StationDAO;
 import com.acc.internship.repo.UserDAO;
+import com.acc.internship.repo.UserRoleDAO;
 
 @Controller
 public class AdminController {
@@ -23,6 +26,9 @@ public class AdminController {
 		
 		@Autowired
 		private UserDAO userDao;
+		
+		@Autowired
+		private UserRoleDAO userRoleDao;
 		
 		@RequestMapping(value="/admin?routes")
 		public String adminRoutes(Model model){
@@ -37,14 +43,30 @@ public class AdminController {
 			Station s = new Station();
 			model.addAttribute("station",s);
 			
+			User user = new User();
+			model.addAttribute("user",user);
+			
 			return "admin";
 		}
 		
-	   @RequestMapping(value="/admin", method=RequestMethod.POST)
+	   @RequestMapping(value="/admin/newstation", method=RequestMethod.POST)
 	    public String stationSubmit(@ModelAttribute Station station, Model model) {
+		   
 	       	stationDao.add(station);
 	       	//model.addAttribute("station", station);
 	        return "admin";
+	    }
+	   
+		
+	   @RequestMapping(value="/admin/adduser", method=RequestMethod.POST)
+	    public String userSubmit(@ModelAttribute User user, Model model) {
+
+		   	user.setRole(userRoleDao.list().get(1));
+		   	String pass = new BCryptPasswordEncoder().encode(user.getPassword());
+		   	user.setPassword(pass);
+	       	userDao.add(user);
+	       	//model.addAttribute("station", station);
+	        return "redirect:/admin?adduser";
 	    }
 	   
 	   
---
+++ hello/src/main/java/com/acc/internship/repo/UserRoleDAO.java
@@ -0,0 +1,9 @@
+package com.acc.internship.repo;
+
+import java.util.List;
+
+import com.acc.internship.model.UserRole;
+
+public interface UserRoleDAO {
+	public List<UserRole> list();
+}
