1bbb1d9ec3311312ec7b45ec2c2cf8007e4c6739
Cristian Groza <cristy1126@gmail.com>
2015-08-24 13:11:11 +0300

    merge

--- hello/src/main/java/com/acc/internship/controller/HomeController.java
+++ hello/src/main/java/com/acc/internship/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.acc.internship.model.Route;
@@ -50,10 +51,7 @@ public class HomeController {
 		return "driver";
 	}
 	
-	@RequestMapping("/test")
-	public String xxx(Model model){
-		return "test";
-	}
+	
 	
 	
 	
