1f4c0c59d2ab78d2e6d6bef38c82c20ba40bfea4
razvantufisi <razvantufisi94@yahoo.ro>
2015-08-28 14:58:54 +0300

    actions

---
+++ hello/src/main/webapp/js/clock.js
@@ -0,0 +1,20 @@
+var timeStr, dateStr;
+
+function myFunction(x) {
+	now = new Date();
+	timer="time"+x;
+	hours = now.getHours();
+	minutes = now.getMinutes();
+	seconds = now.getSeconds();
+	
+	  date= now.getDate();
+      month= now.getMonth()+1;
+      year= now.getFullYear();
+    dateStr= "" +year+"-"+month+"-"+date;
+	timeStr = "" + hours + ":" + minutes + ":" + seconds;
+	
+	timeStamp=""+dateStr+" "+timeStr;
+	
+	document.getElementById(timer).value=timeStamp;
+
+}
