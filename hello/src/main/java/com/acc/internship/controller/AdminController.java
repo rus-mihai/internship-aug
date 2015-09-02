bd5ba5c90240c7d7b9c40c3bbda552227beb49e6
Cristian Groza <cristy1126@gmail.com>
2015-09-02 12:23:38 +0300

    Database records table automatic feed working

--- hello/src/main/java/com/acc/internship/controller/AdminController.java
+++ hello/src/main/java/com/acc/internship/controller/AdminController.java
@@ -1,16 +1,30 @@
 package com.acc.internship.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.acc.internship.model.Record;
+import com.acc.internship.model.RecordFeeding;
+import com.acc.internship.repo.RecordDAO;
+import com.acc.internship.repo.RouteDAO;
+
+
 
 @Controller
 public class AdminController {
+	
+	
+	@Autowired
+	private RecordDAO recordDao;
+	
+	@Autowired
+	private RouteDAO routeDao;
+	
 	
 	@PreAuthorize("hasAuthority('admin')")
 	@RequestMapping(value = "/admin")
@@ -31,5 +45,20 @@ public class AdminController {
 		
 		return "realtime";
 	}
+	
+	@RequestMapping(value = "/admin/feed")
+	public String feedRecords(Model model){
+		
+		RecordFeeding recordFeeding = new RecordFeeding(routeDao);
+		for(int i=0; i<100; i++){
+			Record r = recordFeeding.getRecord();
+			recordDao.add(r);
+		}
+		
+		model.addAttribute("page", "routes");
+		return "admin";
+	}
+	
+	
 	
 }
---
+++ hello/src/main/java/com/acc/internship/model/RecordFeeding.java
@@ -0,0 +1,84 @@
+package com.acc.internship.model;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.acc.internship.repo.RouteDAO;
+
+public class RecordFeeding {
+	
+	private RouteDAO routeDao;
+	private Route route;
+	private int MIN = 10;
+	private int MAX = 40;
+	
+	public RecordFeeding(RouteDAO routeDao){
+		route = routeDao.get(1);
+		this.routeDao = routeDao;
+	}
+	
+	public Record getRecord(){
+		int duration = randBetween(MIN, MAX);
+		Date start = randomDate();
+		Date intermediar = getNext(start,duration);
+		
+		duration = randBetween(MIN,MAX);
+		Date end = getNext(intermediar,duration);
+		Record r = new Record();
+		r.setStart(start);
+		r.setPause(intermediar);
+		r.setStop(end);
+		r.setRouterecord(route);
+		
+		return r;
+	}
+	
+	public Date randomDate(){
+		int year = randBetween(2014, 2015);
+		int month = randBetween(0,11);
+		int hour = randBetween(0, 23);
+		int min = randBetween(0, 59);
+		int sec = randBetween(0, 59);
+		
+		GregorianCalendar gc = new GregorianCalendar(year, month, 1);
+		int day = randBetween(1, gc.getActualMaximum(gc.DAY_OF_MONTH));
+		
+		gc.set(year, month, day, hour, min, sec);
+		return gc.getTime();
+	}
+	
+	public Date getNext(Date date, int duration){
+		
+		GregorianCalendar gc = new GregorianCalendar();
+		gc.setTime(date);
+		
+		gc.add(gc.MINUTE, duration);
+		
+		return gc.getTime();
+		
+	}
+	
+	public static int randBetween(int start, int end){
+		return start + (int)Math.round(Math.random() * (end - start));
+	}
+
+	public int getMIN() {
+		return MIN;
+	}
+
+	public void setMIN(int mIN) {
+		MIN = mIN;
+	}
+
+	public int getMAX() {
+		return MAX;
+	}
+
+	public void setMAX(int mAX) {
+		MAX = mAX;
+	}
+	
+	
+}
