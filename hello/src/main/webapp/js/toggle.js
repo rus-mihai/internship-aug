8a84e2137953311ffcc8719231cde73200c1e083
razvantufisi <razvantufisi94@yahoo.ro>
2015-09-09 10:19:18 +0300

    toogle chart

---
+++ hello/src/main/webapp/js/toggle.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#button2").click(function(){
+        $("#retour").toggle(1000);
+    });
+    
+    $("#button1").click(function(){
+        $("#tour").toggle(1000);
+    });
+});
